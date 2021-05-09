1730e76683252e0d8c19acfd17c56cc149230d54
skyegrey <dev.skyegrey@gmail.com>
2021-05-09 18:12:55 -0500

    + Created new react app + Added the boilerplate code

---
+++ chapter-14/ipaddress/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import IPAddressContainer from "./IPAddressContainer"
+
+const destination = document.querySelector("#container")
+
+ReactDOM.render(
+    <div>
+        <IPAddressContainer />
+    </div>,
+    destination
+)
