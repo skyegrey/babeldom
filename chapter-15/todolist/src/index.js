28fb8fdc6f7f9eed723055e8652cce11f21533c5
skyegrey <dev.skyegrey@gmail.com>
2021-05-10 15:07:23 -0500

    + Index now loads the TodoList component
    + Todo list now holds state of entered items internally

--- chapter-15/todolist/src/index.js
+++ chapter-15/todolist/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"
+import TodoList from "./TodoList";
 
 const destination = document.querySelector("#container")
 
 ReactDOM.render(
     <div>
-        <p>Hello!</p>
+        <TodoList />
     </div>,
     destination
 )
