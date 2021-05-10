94133a3e1197975e6fa3a8c2eeff8a53353a0244
skyegrey <dev.skyegrey@gmail.com>
2021-05-10 14:55:26 -0500

    + Boilerplate code for app

---
+++ chapter-15/todolist/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+
+const destination = document.querySelector("#container")
+
+ReactDOM.render(
+    <div>
+        <p>Hello!</p>
+    </div>,
+    destination
+)
