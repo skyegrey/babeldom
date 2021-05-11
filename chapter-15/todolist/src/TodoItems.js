e14d440089bea0034bd79f772b41f5cd07a162b9
skyegrey <dev.skyegrey@gmail.com>
2021-05-11 13:00:40 -0500

    + Now deletes a given task on click

--- chapter-15/todolist/src/TodoItems.js
+++ chapter-15/todolist/src/TodoItems.js
@@ -3,12 +3,17 @@ import React, { Component } from "react";
 class TodoItems extends Component {
     constructor(props) {
         super(props);
-
+        
+        this.delete = this.delete.bind(this)
         this.createTask = this.createTask.bind(this)
     }
 
     createTask(item) {
-        return <li key={item.key}>{item.text}</li>
+        return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+    }
+
+    delete(key) {
+        this.props.delete(key)
     }
 
     render() {
