0cacd233e0bc46c5a49b1991d2bbcb8fa599a6ae
skyegrey <dev.skyegrey@gmail.com>
2021-05-12 13:36:04 -0500

    + Sliding menu now flys out :D

--- chapter-16/slidingmenu/src/MenuButton.js
+++ chapter-16/slidingmenu/src/MenuButton.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import './MenuButton.css'
 
 class MenuButton extends Component {
+
     render() {
         return(
             <button id="roundButton"
-                    onMouseDown={this.props.handleMouseDown}
+                    onMouseDown={this.props.handleMousedown}
                     />
         )
     }
--- chapter-16/slidingmenu/src/MenuContainer.js
+++ chapter-16/slidingmenu/src/MenuContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import MenuButton from "./MenuButton";
+import Menu from "./Menu"
 
 class MenuContainer extends Component {
     constructor(props) {
@@ -30,6 +31,8 @@ class MenuContainer extends Component {
         return(
             <div>
                 <MenuButton handleMousedown={this.handleMouseDown} />
+                <Menu handleMouseDown={this.handleMouseDown}
+                      menuVisibility={this.state.visible} />
                 <div>
                     <p>Can you spot the item that doesn't belong?</p>
                     <ul>
