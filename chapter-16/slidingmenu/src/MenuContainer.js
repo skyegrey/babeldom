0d453224e146d67b170b4b1617a2dcffadf3a036
skyegrey <dev.skyegrey@gmail.com>
2021-05-12 13:14:38 -0500

    + Added event handling for displaying the menu

--- chapter-16/slidingmenu/src/MenuContainer.js
+++ chapter-16/slidingmenu/src/MenuContainer.js
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
 
 class MenuContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visible: false
+        }
+
+        this.toggleMenu = this.toggleMenu.bind(this)
+        this.handleMouseDown = this.handleMouseDown.bind(this)
+    }
+
+    toggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+    handleMouseDown(e) {
+        this.toggleMenu()
+
+        console.log("clicked")
+        e.stopPropagation()
+    }
+
     render() {
         return(
             <div>
